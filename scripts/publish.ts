527a5d358d879f8c63d874979ee435e29f05b440
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2023-06-16 16:21:14 +0300

    remove-top-level-await-2nd (#25)

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,18 +1,19 @@
 import { getPackages, publishPackage, step } from "./releaseUtils";
 
-const packages = await getPackages();
-for (const pkg of packages) {
-    try {
-        step({ msg: `Starting publish of ${pkg.parsed.name}...` });
-        if (!pkg.parsed.version) {
-            throw new Error("Missing pkg version!");
+(async () => {
+    const packages = await getPackages();
+    for (const pkg of packages) {
+        try {
+            step({ msg: `Starting publish of ${pkg.parsed.name}...` });
+            if (!pkg.parsed.version) {
+                throw new Error("Missing pkg version!");
+            }
+
+            publishPackage(pkg.path);
+            step({ msg: `Finished publishing of ${pkg.parsed.name}` });
+        } catch (err) {
+            step({ msg: `Error on publishing ${pkg.parsed.name}`, err });
+            process.exit(1);
         }
-
-        publishPackage(pkg.path);
-        step({ msg: `Finished publishing of ${pkg.parsed.name}`} );
-
-    } catch (err) {
-        step({ msg: `Error on publishing ${pkg.parsed.name}`, err });
-        process.exit(1);
     }
-}
+})();
